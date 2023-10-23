11eb68d396d1fdd61cefb35fcae68c76f3c31822
Swamy718 <144110970+Swamy718@users.noreply.github.com>
2023-10-23 13:05:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import About_me from './components/About_me';
-import { HashRouter, Route, Routes } from 'react-router-dom/dist/umd/react-router-dom.development';
-import Nav_Bar from './components/Nav_Bar';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Education from './components/Education';
-// import Word_count from './components/Word_count';
-// // import Color_picker from './components/Color_picker';
-// // import Dummy_data from './components/Dummy_data';
-// import Age_calculator from './components/Age_calculator';
-// import MyComponent from './components/MyComponent';
+import Age_calculator from './components/Age_calculator';
 
 function App() {
-  // const arr=["red","lawngreen","blue","yellow","fuchsia","skyblue","coral","darkmagenta","pink","green","orangered","deepskyblue","brown","lightcoral","darkcyan","goldenrod"]
   return (
     <div className="App">
-       {/* <Color_picker color={arr}/> */}
-       {/* <Dummy_data/> */}
-       {/* <Age_calculator/> */}
-       {/* <MyComponent/> */}
-       <HashRouter>
-        <Nav_Bar/>
-        <Routes>
-          <Route path="/" element={<About_me/>}></Route>
-          <Route path="/education" element={<Education/>}></Route>
-          <Route path="/skills" element={<Skills/>}></Route>
-          <Route path="/projects" element={<Projects/>}></Route>
-        </Routes>
-        <Contact/>
-       </HashRouter>
-
+    <Age_calculator/>
     </div>
   );
 }
